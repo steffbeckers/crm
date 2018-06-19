c983bdb834b403470be386b835d9116a4ef95b62
Steff Beckers <steff@steffbeckers.eu>
2018-06-19 22:52:52 +0200

    vue-i18n setup

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -4,12 +4,6 @@
       <v-layout column align-center>
         <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
         <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;Steff</em>
-            </small>
-          </footer>
         </blockquote>
       </v-layout>
     </v-slide-y-transition>
--- vue/src/main.js
+++ vue/src/main.js
@@ -5,8 +5,27 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+// Steff
+import VueI18n from 'vue-i18n'
 
 Vue.use(Vuetify)
+// Steff
+Vue.use(VueI18n)
+
+// i18n
+const translations = {
+  en: {
+    account: 'Account',
+    contact: 'Contact'
+  }
+}
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'nl',
+  fallbackLocale: 'en',
+  translations
+})
 
 Vue.config.productionTip = false
 
@@ -14,6 +33,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  i18n,
   components: { App },
   template: '<App/>'
 })
