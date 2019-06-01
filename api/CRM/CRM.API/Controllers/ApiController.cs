3a06ff644ccb8ddb74a16539d10768d3eff19bee
Steff Beckers <steff@steffbeckers.eu>
2019-06-01 13:47:26 +0200

    API controller added

---
+++ api/CRM/CRM.API/Controllers/ApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        public static DateTime Started { get; } = DateTime.UtcNow;
+        public static string Version { get; } = typeof(ApiController).Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version;
+
+        [HttpGet]
+        public IActionResult Info()
+        {
+            dynamic info = new ExpandoObject();
+            info.Started = Started;
+            info.Version = Version;
+
+            return Ok(info);
+        }
+    }
+}
