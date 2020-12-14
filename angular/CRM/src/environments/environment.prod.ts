4b4fcb976b52ce99cdceb22cf3375e75c279e594
Steff Beckers <steff@steffbeckers.eu>
2020-12-15 00:06:53 +0100

    Reset secret and removed from source

--- angular/CRM/src/environments/environment.prod.ts
+++ angular/CRM/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   hmr: false,
   api: 'https://crm.steffbeckers.eu/api',
-  'maps-api-key': 'AIzaSyDOlsrcJI5GAKhH9oFvFH5aqjMd3vxYtc0',
+  'maps-api-key': 'SECRET',
 };
